de8c5b8c1e19a05ab5cf5ed6b50aef20544cd7a5
Tasha1003Natasha <0955712824n@gmail.com>
2022-09-13 20:29:51 +0300

    Нome готово

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,8 +14,8 @@ import SocialsIcons from 'components/Navbar/SocialsIcons';
 const Home = () => {
   return (
     <>
-      <div className="w-full h-full p-10 md:p-20 lg:p-40 bg-gradient-to-l from-white to-[#adbbc5] hover:bg-gradient-to-r  dark:from-white dark:to-[#0d1c2c]">
-        <div className="lg:max-w-[1000px]  mx-auto bg-[#adbbc5] text-[#000080]  dark:bg-[#0d1c2c] dark:text-[#8892b0]">
+      <div className="w-full h-screen p-12 md:p-20 bg-gradient-to-l from-white to-[#adbbc5] hover:bg-gradient-to-r  dark:from-white dark:to-[#0d1c2c]">
+        <div className="lg:max-w-[1200px]  mx-auto bg-[#adbbc5] text-[#000080]  dark:bg-[#0d1c2c] dark:text-[#8892b0]">
           <div className="flex flex-col md:flex-row md:items-center items-center">
             <div className="flex flex-col justify-center items-center">
               <p className="text-gray-50 font-bold lg:text-2xl">
@@ -28,7 +28,7 @@ const Home = () => {
               <h2 className="text-lg sm:text-3xl md:text-2xl lg:text-4xl leading-relaxed md:leading-snug mb-2 text-[#000080]  dark:text-[#ccd6f6]">
                 I'm a Frontend Developer.
               </h2>
-              <p className="m-2 md:mx-4 sm:m-0 text-center text-sm sm:text-base md:text-xl lg:text-2xl text-gray-50 mb:mb-4">
+              <p className="mx-4  text-center text-sm sm:text-base md:text-xl lg:text-2xl text-gray-50 mb:mb-4">
                 I am passionate about creating websites that improve the lives
                 of those around me.
               </p>
