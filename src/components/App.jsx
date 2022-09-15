d5848cd842f2b878aeccbdac3b26e0dfd18ebc36
Tasha1003Natasha <0955712824n@gmail.com>
2022-09-15 14:50:46 +0300

    netlify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import Layout from './Layout/Layout';
-import Home from '../Pages/Home';
-import Contact from '../Pages/Contact';
-import Work from '../Pages/Work';
-import Skills from '../Pages/Skills';
-import About from '../Pages/About';
-import NotFound from 'Pages/NotFound';
+// import Home from '../Pages/Home';
+// import Contact from '../Pages/Contact';
+// import Work from '../Pages/Work';
+// import Skills from '../Pages/Skills';
+// import About from '../Pages/About';
+// import NotFound from 'Pages/NotFound';
 import { Routes, Route } from 'react-router';
+
+import { lazy, Suspense } from 'react';
+const HomeLazy = lazy(() => import('../Pages/Home'));
+const ContactLazy = lazy(() => import('../Pages/Contact'));
+const WorkLazy = lazy(() => import('../Pages/Work'));
+const SkillsLazy = lazy(() => import('../Pages/Skills'));
+const AboutLazy = lazy(() => import('../Pages/About'));
+const NotFoundLazy = lazy(() => import('../Pages/NotFound'));
 
 export const App = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<HomeLazy />} />
+          <Route path="/contact" element={<ContactLazy />} />
+          <Route path="/work" element={<WorkLazy />} />
+          <Route path="/skills" element={<SkillsLazy />} />
+          <Route path="/about" element={<AboutLazy />} />
+          <Route path="*" element={<NotFoundLazy />} />
         </Route>
       </Routes>
-    </>
+    </Suspense>
   );
 };
